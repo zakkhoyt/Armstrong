1aafe928d8af141417b87fda2cf017111f94f4c6
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-05-12 18:12:16 -0700

    first commit

---
+++ Armstrong/DayOneTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  DayOneTableViewCell.m
+//  Armstrong
+//
+//  Created by Zakk Hoyt on 5/12/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import "DayOneTableViewCell.h"
+
+@implementation DayOneTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
