4a60fa410bea05855098b40bf534254325d8126d
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-05-12 17:52:53 -0700

    Initial Commit

---
+++ Armstrong/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Armstrong
+//
+//  Created by Zakk Hoyt on 5/12/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
